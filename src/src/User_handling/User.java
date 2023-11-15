4fec0aebbb182fd6401c3a1f66e727a85b645564
Janasobeih <123209222+Janasobeih@users.noreply.github.com>
2023-11-15 12:24:05 +0200

    User class commit

--- src/src/User_handling/User.java
+++ src/src/User_handling/User.java
@@ -32,7 +32,6 @@ public class User {
     public void setUsername(String username) {
         this.username = username;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
