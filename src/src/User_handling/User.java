21b8b93735b4bc1000b11582e86e21d3046da639
Janasobeih <123209222+Janasobeih@users.noreply.github.com>
2023-11-15 12:22:25 +0200

    User class commit

---
+++ src/src/User_handling/User.java
@@ -0,0 +1,48 @@
+package User_handling;
+
+public class User {
+    String name;
+    String username;
+    String password;
+    String phoneNumber;
+
+    //Account account;
+
+       public String getName() {
+           return name;
+       }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+  /*  public void setAccount(Account account) {
+        this.account = account;
+    }
+    public account getAccount(){return account;}*/
+
+}
